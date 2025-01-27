ab5182c01bfdc4d5a8c1086d0f11b91eab72d51c
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-01-27 09:36:58 -0300

    :sparkles: Feat: Update app.component.ts
    Adicionando funcionalidades

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -4,10 +4,21 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, FormsModule],
+  //templateUrl: './app.component.html',
+  template:
+      <main class="main">
+          <div class="content">
+            <div class="left-side">
+              <h1>Hello, {{ name }}</h1>
+              <label for="name">Name: </label>
+              <input [(ngModel)]="name">
+            </div>
+          </div>
+      </main>
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular';
+  name = ''
 }
