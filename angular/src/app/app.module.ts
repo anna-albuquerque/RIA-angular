ab1f13ba3cd582dfedbb7a5f48e76040708af1df
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-02-01 00:57:48 +0000

    Chore: app module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms'; // Para usar [(ngModel)]
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
@@ -8,12 +9,18 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox'; // Para <mat-checkbox>
+
+import { AppComponent } from './app.component';
 
 @NgModule({
+  declarations: [
+    AppComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatTableModule,
     MatButtonModule,
     MatDialogModule,
@@ -21,7 +28,9 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatSlideToggleModule,
     MatIconModule,
-    FormsModule
-  ]
+    MatCheckboxModule // Adicionando MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
