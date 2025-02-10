08393f46439307781def04e2b45fd24c4bbf20ce
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-02-10 13:05:02 +0000

    :wrench: Chore: ajuste routes

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { OwnerCrudComponent } from './owner-crud.component';
+import { OwnerListComponent } from './owner-list.component';
+import { OwnerInsertComponent } from './owner-insert.component';
 import { HelloComponent } from './hello.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'owners', pathMatch: 'full' },  // Redireciona para a página de owners
-  { path: 'hello', component: HelloComponent },
-  { path: 'owners', component: OwnerCrudComponent }
+  { path: '', redirectTo: 'owners', pathMatch: 'full' },
+  { path: '**', redirectTo: 'owners'},  // Redireciona para a página de owners
+  { path: 'Hello', component: HelloComponent },
+  { path: 'Owners', component: OwnerCrudComponent },
+  { path:  'OwnersInsertComponet', component: OwnerInsertComponent},
+  { path:  'OwnersListComponet', component: OwnerListComponent}
 ];
