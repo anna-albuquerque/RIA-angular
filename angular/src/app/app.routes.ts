08d1c31534293c9ca2ff142520a6c687312856f6
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-02-10 13:05:21 +0000

    :wrench: Chore: ajuste routes

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { OwnerInsertComponent } from './owner-insert.component';
 import { HelloComponent } from './hello.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'owners', pathMatch: 'full' },
-  { path: '**', redirectTo: 'owners'},  // Redireciona para a página de owners
+  { path: '', redirectTo: 'owners', pathMatch: 'full' }, // página exatamente até o "/"
+  { path: '**', redirectTo: 'owners'},  // Redireciona para a página de owners batida salve todos
   { path: 'Hello', component: HelloComponent },
   { path: 'Owners', component: OwnerCrudComponent },
   { path:  'OwnersInsertComponet', component: OwnerInsertComponent},
