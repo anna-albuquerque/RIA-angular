af7e57717d20b433a835d11bf91f28f5e879933a
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-02-24 15:12:01 +0000

    Chore: models

--- owner_backend/owner_api/models.py
+++ owner_backend/owner_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    age = models.IntegerField()
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
