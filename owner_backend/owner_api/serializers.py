fdb9415cdbd617659af15666ed67e06751b3640c
Anna Carolinne Albuquerque de Medeiros <70245734+anna-albuquerque@users.noreply.github.com>
2025-02-24 15:13:31 +0000

    Chore: serializer

---
+++ owner_backend/owner_api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Owner
+
+class OwnerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Owner
+        fields = ['id', 'name', 'age', 'is_active']
